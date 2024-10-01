f436d6c48f7c2972fda4c421b16e821dadd237bf
i-OmSharma <sharmaom1201@gmail.com>
2024-10-01 20:05:23 +0530

    first java code in  2022

---
+++ JAVA/firstCode/src/primitives.java
@@ -0,0 +1,16 @@
+public class primitives {
+    public static void main(String[] args) {
+        int rollno=21;
+        char letter='O';// char starts with single quote
+        float number=27.7f;
+        double largedecimalnumbers=15643.16843;
+        long largeint=684130681651483155L;//ends with upper class L
+        boolean check=true;//check weather true or false
+
+
+        //its not primitives
+        String name= "om";
+
+        /*last data type in which u cant break into any other data type furthur is called primitives*/
+    }
+}
