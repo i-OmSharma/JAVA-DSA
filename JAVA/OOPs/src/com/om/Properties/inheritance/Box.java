eb629fd3748b4a667920334a610441d01767354a
i-OmSharma <sharmaom1201@gmail.com>
2024-10-24 21:57:37 +0530

    OOP's Inheritance

---
+++ JAVA/OOPs/src/com/om/Properties/inheritance/Box.java
@@ -0,0 +1,39 @@
+package com.om.Properties.inheritance;
+
+public class Box {
+    private double l;
+    double w;
+    double h;
+
+    Box () {
+         this.h = -1;
+         this.l = -1;
+         this.w = -1;
+    }
+    // cube
+
+    Box ( double side) {
+
+        // super();//Object class
+
+        this.w = side;
+        this.l = side;
+        this.h = side;
+    }
+
+    Box (double l, double h, double w) {
+        this.l = l;
+        this.w = w;
+        this.h = h;
+    }
+    
+    Box (Box old) {
+         this.h = old.h;
+         this.l = old.l;
+         this.w = old.w;
+    }
+
+    public void information () {
+        System.out.println("Running the Box");
+    }
+}
---
+++ JAVA/OOPs/src/com/om/Properties/inheritance/BoxWeight.java
@@ -0,0 +1,30 @@
+//every class has Object as a superClass
+//super directly above its one(C->B->A)
+//u can use super instead of this, if u want to explicitly refer something over in the parent class
+//in the hierarchy of classes, if the super class constructor require some parameters then all the subclass must pass those parameters, up the line
+//super class is initialised first, subclass care about the parent class, subclass require prerequisite of parent class and the parent class has no  knowledge of the subclass
+//super class variable can be used to reference any object to that derived class , hence we are able to pass BoxWeight object to the Boc constructor
+package com.om.Properties.inheritance;
+
+public class BoxWeight extends Box {
+    double weight;
+
+    BoxWeight () {
+        this.weight = -4;
+//        this.l; can't use it coz its private.
+    }
+
+    BoxWeight(BoxWeight other ) {
+        super(other);
+        weight = other.weight;
+    }
+
+    BoxWeight(double l, double h, double w, double weight) {
+        super(l,h,w);//cal the parent class constructor
+        this.weight = weight;
+    }
+}
+
+//the subclass include all the member of its parent class or superclass, it will not be able to access those member of the
+// parent class that have been declared as "privet".
+
