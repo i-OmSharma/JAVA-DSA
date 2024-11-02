20a5ff48f93e8e740ab66a3f61ef6da19d013bfe
i-OmSharma <sharmaom1201@gmail.com>
2024-11-02 19:31:54 +0530

    Polymorphism

---
+++ JAVA/OOPs/src/com/om/Properties/polymorphism/Main.java
@@ -0,0 +1,15 @@
+package com.om.Properties.polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes shape = new Shapes();
+        Circle circle = new Circle();
+        //Shapes circle = new Circle();//whats the type of the object that will be only at the use
+        Square square = new Square();
+        Triangle triangle = new Triangle();
+
+        circle.area();
+    }
+}
+
+//what variable it has access depends on the ref variable.
