949f9a999ea657b47b4026823feee4b195f676cb
i-OmSharma <sharmaom1201@gmail.com>
2024-11-03 19:11:57 +0530

    Polymorphism

---
+++ JAVA/OOPs/src/com/om/Properties/polymorphism/Numbers.java
@@ -0,0 +1,17 @@
+package com.om.Properties.polymorphism;
+
+public class Numbers {
+    int sum(int a, int b) {
+        return a + b;
+    }
+    int sum(int a, int  b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        Numbers obj = new Numbers();
+        obj.sum(1,2);
+        obj.sum(1,2,3);
+//        obj.sum(1,2,3,4);
+    }
+}
