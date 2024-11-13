e9d3383136c03ef1e082d455e4c620f089929632
i-OmSharma <sharmaom1201@gmail.com>
2024-11-13 20:26:00 +0530

    Access

---
+++ JAVA/OOPs/src/com/om/access/Main.java
@@ -0,0 +1,15 @@
+package com.om.access;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(10,"Om");
+        //1. Access the data Members
+        //2. Modify the data members
+
+        System.out.println(obj.getNum());
+
+        obj.setNum(12);
+
+        System.out.println(obj.getNum());
+    }
+}
