b6f1c9af53d02d4a1996b0982e97322d76bad634
i-OmSharma <sharmaom1201@gmail.com>
2024-11-21 19:42:12 +0530

    Updated ObjectDemo.java

--- JAVA/OOPs/src/com/om/access/ObjectDemo.java
+++ JAVA/OOPs/src/com/om/access/ObjectDemo.java
@@ -1,4 +1,47 @@
 package com.om.access;
 
 public class ObjectDemo {
+
+    int num;
+
+    // Already disused
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+
+    }
+//-------------------------------------------------------------------------------------------//
+    public ObjectDemo(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public int hashCode() {  //gives number representation of an object
+        return super.hashCode(); //random number using algorithm
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+
+
+    public ObjectDemo() {
+        super();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public static void main(String[] args) {
+        ObjectDemo obj = new ObjectDemo(12);
+        System.out.println(obj.hashCode());
+    }
 }
