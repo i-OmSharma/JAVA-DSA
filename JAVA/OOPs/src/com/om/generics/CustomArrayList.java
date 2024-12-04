ef48582aa70f869cf51bbcf8fe8361ed1cea032f
i-OmSharma <sharmaom1201@gmail.com>
2024-12-04 18:57:04 +0530

    coustom arrylist integer type

--- JAVA/OOPs/src/com/om/generics/CustomArrayList.java
+++ JAVA/OOPs/src/com/om/generics/CustomArrayList.java
@@ -1,6 +1,7 @@
 package com.om.generics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 // Working of ArrayList
 // when the size exceeds, the arraylist make a new arraylist of size double of the existing one,
@@ -9,23 +10,73 @@ import java.util.ArrayList;
 // its taking constant time on an average (Amortized constant time).
 
 public class CustomArrayList {
+
+    private int[] data;
+    private static int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    public CustomArrayList() {
+        this.data = new int[DEFAULT_SIZE];
+    }
+
+    public void add (int num) {
+        if (isFull()) {
+            resize();
+        }
+        data[size++] = num;
+    }
+
+    private void resize() {
+        int[] temp =  new int[data.length * 2];
+
+        //copy the current item into the new array
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
+    }
+
+    public int remove() {
+        int removed = data[--size];
+        return removed;
+    }
+
+    public int get(int index) {
+        return data[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void set(int index, int value) {
+        data[index] = value;
+    }
+
+    private boolean isFull() {
+        return size == data.length;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomArrayList{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
+    }
+
+
     public static void main(String[] args) {
+//        ArrayList list = new ArrayList();
+        CustomArrayList list = new CustomArrayList();
+//        list.add(3);
+//        list.add(5);
+//        list.add(9);
 
-        private int[] data;
-        private int DEFAULT_SIZE = 10;
-        private int size = 0;
-
-        public CustomArrayList() {
-            this.data = new int[DEFAULT_SIZE];
+        for(int i = 0; i < 14; i++){
+            list.add(2 * i);
         }
 
-        public void add (int num) {
-            if (isFull()) {
-                resize();
-            }
-        }
-
-
-        ArrayList list = new ArrayList();
+        System.out.println(list);
     }
 }
