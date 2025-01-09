9c352ec98bc6abe67f5b2ddc3d575938d7f6ef38
i-OmSharma <sharmaom1201@gmail.com>
2025-01-09 17:21:32 +0530

    collections

---
+++ JAVA/OOPs/src/com/om/collections/Main.java
@@ -0,0 +1,19 @@
+package com.om.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+
+        System.out.println(list2);
+
+    }
+}
