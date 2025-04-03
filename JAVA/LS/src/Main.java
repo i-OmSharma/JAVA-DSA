b32b458ed48a44408dd0ea763f26538f51ad3159
i-OmSharma <sharmaom1201@gmail.com>
2025-04-03 20:30:44 +0530

    Linear search

--- JAVA/LS/src/Main.java
+++ JAVA/LS/src/Main.java
@@ -3,3 +3,8 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+// Linear Search - Says start searching from first till u get what you're looking for.
+// If no value found return -1, value not found not exist.
+// Time complexity: Best:  O(1) constant
+//                  Worst: O(n) n = size of array
