c8125de5b8b8ed30ddb90f94d9315b9216364f35
i-OmSharma <sharmaom1201@gmail.com>
2026-02-07 23:07:49 +0530

    HashSet

--- JAVA/HashMaps/src/com/om/HashSetDemo.java
+++ JAVA/HashMaps/src/com/om/HashSetDemo.java
@@ -1,2 +1,18 @@
-package com.om;public class HashSetDemo {
+package com.om;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(21);
+        set.add(33);
+        set.add(52);
+        set.add(91);
+        set.add(87);
+        set.add(65);
+
+        System.out.println(set);
+    }
 }
