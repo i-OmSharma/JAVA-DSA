e71f2318c33e8b4bdf07c21a77988822ffe45e4c
i-OmSharma <sharmaom1201@gmail.com>
2026-02-07 23:08:08 +0530

    TreeSet

--- JAVA/HashMaps/src/com/om/TreeSetDemo.java
+++ JAVA/HashMaps/src/com/om/TreeSetDemo.java
@@ -1,2 +1,18 @@
-package com.om;public class TreeSetDemo {
+package com.om;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet<Integer> set = new TreeSet<>();
+
+        set.add(21);
+        set.add(33);
+        set.add(52);
+        set.add(91);
+        set.add(87);
+        set.add(65);
+
+        System.out.println(set);
+    }
 }
