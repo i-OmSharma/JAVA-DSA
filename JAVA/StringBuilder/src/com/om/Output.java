3d5a83d6acb8ccd868354e80c3551465e9bf250a
i-OmSharma <sharmaom1201@gmail.com>
2026-02-18 22:54:47 +0530

    String review

---
+++ JAVA/StringBuilder/src/com/om/Output.java
@@ -0,0 +1,11 @@
+package com.om;
+
+import java.util.Arrays;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(56);
+        System.out.println("Om");
+        System.out.println(Arrays.toString(new int[] {1,2,3,4,5}));
+    }
+}
---
+++ JAVA/StringBuilder/src/com/om/PrettyPrinting.java
@@ -0,0 +1,11 @@
+package com.om;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.123f;
+//        System.out.printf("Formatted number is %.2f \n", a );
+
+//        System.out.printf("Pie till 3 %.3f", Math.PI);
+        System.out.printf("Hey I'm %s and I'm the great", "Om");
+    }
+}
