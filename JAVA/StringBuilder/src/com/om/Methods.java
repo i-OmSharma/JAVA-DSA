7a0882ac34c947c5db0e14a6ec7e37aefed13fec
i-OmSharma <sharmaom1201@gmail.com>
2026-02-25 00:00:12 +0530

    string Data

---
+++ JAVA/StringBuilder/src/com/om/Methods.java
@@ -0,0 +1,10 @@
+package com.om;
+
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args) {
+        String name = "Om";
+        System.out.println(name.toCharArray());
+    }
+}
--- JAVA/StringBuilder/src/com/om/Operators.java
+++ JAVA/StringBuilder/src/com/om/Operators.java
@@ -1,2 +1,16 @@
-package com.om;public class Operator {
+package com.om;
+
+import java.util.ArrayList;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("a" + "b");
+        System.out.println('a' + 'b');
+        System.out.println((char)('a'+3));
+        System.out.println("a"+1);
+        //integer will be converted to Integer that will call toString()
+        System.out.println("om"+ new ArrayList<>());
+
+
+    }
 }
