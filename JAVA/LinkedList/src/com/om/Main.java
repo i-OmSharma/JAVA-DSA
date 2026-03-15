3de86e9584aaa8a9b1f9592e01c08db66a605d1c
i-OmSharma <sharmaom1201@gmail.com>
2026-03-15 18:01:08 +0530

    CircularLinkedList

---
+++ JAVA/LinkedList/src/com/om/Main.java
@@ -0,0 +1,24 @@
+package com.om;
+
+public class Main {
+    public static void main(String[] args) {
+//        LL list = new LL();
+//
+//        list.insertFirst(3);
+//        list.insertFirst(5);
+//        list.insertFirst(8);
+//        list.insertFirst(2);
+//        list.insertlast(1);
+//        list.insert(21,3);
+//        list.display();
+
+
+        CLL list = new CLL();
+        list.insert(2);
+        list.insert(5);
+        list.insert(7);
+        list.insert(1);
+        list.insert(9);
+        list.display();
+    }
+}
